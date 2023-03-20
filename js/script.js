58373bf371f5067e4395b697a7c8fa3f23d0eb33
mirkettinho <mirkodifranco30@gmail.com>
2023-03-20 18:56:08 +0100

    [add] sconto over65

--- js/script.js
+++ js/script.js
@@ -15,18 +15,22 @@ console.log(name, kmpercorsi, age, prezzo_biglietto);
 
 
 
-
+let sconto_minorenni = ""
 if(age < 18) {
-  let sconto_minorenni = ( prezzo_biglietto - ((prezzo_biglietto * 20) /100));
-  console.log( sconto_minorenni);
+   sconto_minorenni = ( prezzo_biglietto - ((prezzo_biglietto * 20) /100));
+  console.log( sconto_minorenni.toFixed(2));
 }
 
+let sconto_over65 = ""
 if(age >= 65){
-  console.log( prezzo_biglietto - ((prezzo_biglietto * 40) /100));
+  sconto_over65 = ( prezzo_biglietto - ((prezzo_biglietto * 40) /100));
+  console.log(sconto_over65.toFixed(2));
 }
 
 
-document.getElementById("output").innerHTML =`Il prezzo del tuo biglietto : è` +  prezzo_biglietto + `€`;
+document.getElementById("output").innerHTML =`Il prezzo del tuo biglietto è: ` +  prezzo_biglietto + `€`;
 
+document.getElementById("output_2").innerHTML =`Il prezzo del tuo biglietto è:  ${sconto_minorenni}€`;
 
+document.getElementById("output_3").innerHTML =`Il prezzo del tuo biglietto è:  ` +  sconto_over65 + `€`;
 
